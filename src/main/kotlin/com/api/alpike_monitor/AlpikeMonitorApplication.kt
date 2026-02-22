d71d9e135bc446242363d599d36d50b4abbc19ab
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2026-02-22 18:06:02 -0300

    spring init

---
+++ src/main/kotlin/com/api/alpike_monitor/AlpikeMonitorApplication.kt
@@ -0,0 +1,11 @@
+package com.api.alpike_monitor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlpikeMonitorApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlpikeMonitorApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/alpike_monitor/AlpikeMonitorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.alpike_monitor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlpikeMonitorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
